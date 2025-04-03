ea504c2d6c33850c076249ca43387a48429d21a8
Yandisa <gaju.yandisa@gmail.com>
2025-04-03 05:01:04 +0200

    Fix: removed duplicate settings.DEBUG media block

--- home/urls.py
+++ home/urls.py
@@ -10,6 +10,3 @@ urlpatterns = [
     path("search/", views.search_results, name="search"),  # Search results
     path("about/", views.about, name="about"),  # Static 'About Us' page
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
